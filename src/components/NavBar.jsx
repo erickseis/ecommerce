39ed81f6fc37efbcf85905e2ba8b4fa85c8d4709
erickseis <erickseis@gmail.com>
2022-10-04 09:08:29 -0300

    carga

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,67 +1,95 @@
 import React, { useState } from 'react';
-import { Navbar, Container, Nav, NavDropdown, Form, Button, Offcanvas } from "react-bootstrap"
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+  Offcanvas,
+} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import CartSidebar from './Cart';
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   const [show, setShow] = useState(false);
-
 
   const handleClose = () => setShow(false);
   const handleShow = () => {
     if (token) {
       setShow(true);
     } else {
-      navigate("/login");
+      navigate('/login');
     }
   };
 
   const logout = () => {
-    localStorage.setItem("token", "");
-    navigate("/login")
-  }
-
+    localStorage.setItem('token', '');
+    navigate('/login');
+  };
 
   return (
     <>
-      <Navbar style={{backgroundColor: '#00FF33', color:'black'}} bg="#00FF33" variant="dark" expand="lg">
-        <Container  fluid>
-          <Navbar.Brand style={{color:'black'}} href="/#/">Home</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
+      <Navbar
+        style={{ backgroundColor: '#23BAC4', color: 'black' }}
+        bg='#23BAC4'
+        variant='dark'
+        expand='lg'
+      >
+        <Container fluid>
+          <Navbar.Brand style={{ color: 'black' }} href='/#/'>
+            Home
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
             <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px', marginLeft: '5rem', display: 'flex', gap: '0.5rem' }}
+              className='me-auto my-2 my-lg-0'
+              style={{
+                maxHeight: '100px',
+                marginLeft: '5rem',
+                display: 'flex',
+                gap: '0.5rem',
+              }}
               navbarScroll
             >
-              <Nav.Link style={{color:'black'}} href="/#/products/:id">products</Nav.Link>
-              <Nav.Link style={{color:'black'}} href="/#/purchase">purchase</Nav.Link>
+              <Nav.Link style={{ color: 'black' }} href='/#/products/:id'>
+                products
+              </Nav.Link>
+              <Nav.Link style={{ color: 'black' }} href='/#/purchase'>
+                purchase
+              </Nav.Link>
 
-            
-
-                {
-                  token ? (<Nav.Link
-                    id='logout'
-                    variant="danger" as={Button} onClick={logout} >log out</Nav.Link>
-                  ) : (
-                    <Nav.Link
-                    style={{color:'black'}}
-                    id='login'  
-                    variant="danger" href="/#/login">login</Nav.Link>
-                  )
-                }
+              {token ? (
                 <Nav.Link
-                  id='cart'
-                  style={{ fontSize: "20px" }}
-                  variant="danger"
+                  id='logout'
+                  variant='danger'
                   as={Button}
-                  onClick={handleShow}>
-                  <i className="fa-solid fa-cart-shopping"></i>
+                  onClick={logout}
+                >
+                  log out
                 </Nav.Link>
-
+              ) : (
+                <Nav.Link
+                  style={{ color: 'black' }}
+                  id='login'
+                  variant='danger'
+                  href='/#/login'
+                >
+                  login
+                </Nav.Link>
+              )}
+              <Nav.Link
+                id='cart'
+                style={{ fontSize: '20px' }}
+                variant='dark'
+                as={Button}
+                onClick={handleShow}
+              >
+                <i className='fa-solid fa-cart-shopping'></i>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -70,7 +98,6 @@ const NavBar = () => {
       <CartSidebar show={show} handleClose={handleClose} />
     </>
   );
-
 };
 
 export default NavBar;
